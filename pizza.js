cdddfafc64df0b6cecc7c354439e060068af658b
Echo Wilkinson <echolamour@gmail.com>
2014-05-12 14:59:23 -0600

    possible javascript

---
+++ pizza.js
@@ -0,0 +1,15 @@
+<!--
+function calculate()
+{
+	var costPerPerson;
+	var totalCost;
+	var people;
+	
+	totalCost = document.getElementById('total_cost').value;
+	people = document.getElementById('number_of_people').value;
+
+	costPerPerson = totalCost/people;
+	
+	document.getElementById('main').innerhtml= costPerPerson;
+}
+-->
