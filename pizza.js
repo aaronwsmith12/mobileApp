3c93ce378a0251e8c3e0cfcf98ee63251c0bee37
Echo Wilkinson <echolamour@gmail.com>
2014-05-14 14:57:08 -0600

    slice math included

--- pizza.js
+++ pizza.js
@@ -4,13 +4,20 @@ function calculate()
 	var costPerPerson;
 	var totalCost;
 	var people;
+	var pizzas;
+	var slicesPerPizzas = 5;
+	var slicesPerPerson;
 	
 	totalCost = document.getElementById('total_cost').value;
 	people = document.getElementById('number_of_people').value;
+	pizzas = document.getElementById('').value;
 
 	costPerPerson = totalCost/people;
+	slicesPerPerson = (pizzas * slicesPerPizzas) / people;
+	
 	
 	document.getElementById('costPerPerson').innerHTML = costPerPerson;
+	document.getElementById('').innerHTML = slicesPerPerson;
 
 }
 -->
