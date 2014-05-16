8c41c40a54ac0bf41c5105d5891655ec89e1a9aa
Aaron Smith <aaronwsmith12@gmail.com>
2014-05-16 14:27:26 -0600

    stupid

--- pizza.js
+++ pizza.js
@@ -1,16 +1,34 @@
-<!--
 function calculate()
 {
 	var costPerPerson;
 	var totalCost;
 	var people;
+	var pizzaSize;
+	var pizzaSlices;
+	var slicesPerPerson;
+	var pizzas;
 	
 	totalCost = document.getElementById('total_cost').value;
 	people = document.getElementById('number_of_people').value;
+	pizzaSize = document.getElementById('pizzaSize').value;
 
 	costPerPerson = totalCost/people;
-	
+
+	slicesPerPerson = (pizzaSlices * pizzas) /people;
+
+	if (pizzaSize == small) {
+		pizzaSlices = 8;
+
+	} else if (pizzaSize == medium) {
+		pizzaSlices = 12;
+
+	} else {
+		pizzaSlices = 16;
+
+	}
+
+
 	document.getElementById('costPerPerson').innerHTML = costPerPerson;
+	
 
 }
--->
