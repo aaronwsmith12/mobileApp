701691a73bf6c954be30616f3ffd9baada74527c
Echo Wilkinson <echolamour@gmail.com>
2014-05-21 14:24:24 -0600

    'js'

--- pizza.js
+++ pizza.js
@@ -3,9 +3,8 @@ function calculate()
 	var costPerPerson;
 	var totalCost;
 	var people;
-<<<<<<< HEAD
 	var pizzas;
-	var slicesPerPizzas = 5;
+	var slicesPerPizzas;
 	var slicesPerPerson;
 	
 	totalCost = document.getElementById('total_cost').value;
@@ -16,7 +15,9 @@ function calculate()
 	slicesPerPerson = (pizzas * slicesPerPizzas) / people;
 	
 	
-=======
+	document.getElementById('costPerPerson').innerHTML = costPerPerson;
+	document.getElementById('').innerHTML = slicesPerPerson;
+	
 	var pizzaSize;
 	var pizzaSlices;
 	var slicesPerPerson;
@@ -25,29 +26,21 @@ function calculate()
 	totalCost = document.getElementById('total_cost').value;
 	people = document.getElementById('number_of_people').value;
 	pizzaSize = document.getElementById('pizzaSize').value;
-	pizzas = document.getElementById('number_of_pizza').value;
 
-	if (document.getElementById('small').checked) {
-  pizzaSlices = 8;
-  	}
+	costPerPerson = totalCost/people;
 
-  	else if (document.getElementById('large').checked) {
-  pizzaSlices = 16;
-  	}
+	slicesPerPerson = (pizzaSlices * pizzas) /people;
 
-  	else {
-  pizzaSlices = 12;
-  	}
+	if (pizzaSize == small) {
+		pizzaSlices = 8;
 
-	costPerPerson = (totalCost/people).toFixed(2);
+	} else if (pizzaSize == large) {
+		pizzaSlices = 16;
 
-	slicesPerPerson = ((pizzaSlices * pizzas) /people).toFixed(0);
+	} else {
+		pizzaSlices = 12;
+	}
 
->>>>>>> a424c8aff4906102cc6ae2a5b05e7479255a96e7
+
 	document.getElementById('costPerPerson').innerHTML = costPerPerson;
-	document.getElementById('').innerHTML = slicesPerPerson;
-
-	document.getElementById('pizzaSize').innerHTML = slicesPerPerson;
-	
-
 }
