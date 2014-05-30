5f817585028589a80b40e437c99ed463313ef3d5
Echo Wilkinson <echolamour@gmail.com>
2014-05-30 14:39:49 -0600

    cleaned up js

--- pizza.js
+++ pizza.js
@@ -6,10 +6,16 @@ function calculate()
 	var pizzas;
 	var slicesPerPizzas;
 	var slicesPerPerson;
+	var pizzaSize;
+	var pizzaSlices;
+	var slicesPerPerson;
+	
 	
 	totalCost = document.getElementById('total_cost').value;
 	people = document.getElementById('number_of_people').value;
 	pizzas = document.getElementById('number_of_pizza').value;
+	pizzaSize = document.getElementsByName('pizzasize').value;
+
 
 	costPerPerson = totalCost/people;
 	slicesPerPerson = (pizzas * slicesPerPizzas) / people;
@@ -17,16 +23,11 @@ function calculate()
 	
 	document.getElementById('costPerPerson').innerHTML = costPerPerson;
 	
-	var pizzaSize;
-	var pizzaSlices;
-	var slicesPerPerson;
-	var pizzas;
-	
-	totalCost = document.getElementById('total_cost').value;
-	people = document.getElementById('number_of_people').value;
-	pizzaSize = document.getElementsByName('pizzasize').value;
 
-	costPerPerson = "Cost Per Person: " + totalCost/people;
+	
+	
+
+	costPerPerson = "Cost Per Person: " + (totalCost/people);
 
 	
 
@@ -42,7 +43,7 @@ function calculate()
 
 	costPerPerson = (totalCost/people).toFixed(2);
 
-	slicesPerPerson = "Slices Per Person" + ((pizzaSlices * pizzas) /people).toFixed(0);
+	slicesPerPerson = "Slices Per Person: " + ((pizzaSlices * pizzas) /people).toFixed(0);
 	
 	document.getElementById('costPerPerson').innerHTML = costPerPerson;
 	document.getElementById('slicePerPerson').innerHTML = slicesPerPerson;
