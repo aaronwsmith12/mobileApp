580ef90c78184657ccd173e03bcd6e68ecb1aa08
Aaron Smith <aaronwsmith12@gmail.com>
2014-06-02 14:57:00 -0600

    added 3 equal signs to equation in js

--- pizza.js
+++ pizza.js
@@ -1,50 +1,49 @@
-function calculate()
-{
-	var costPerPerson;
-	var totalCost;
-	var people;
-	var pizzas;
-	var slicesPerPizzas;
-	var slicesPerPerson;
-	var pizzaSize;
-	var pizzaSlices;
-	var slicesPerPerson;
-	
-	
-	totalCost = document.getElementById('total_cost').value;
-	people = document.getElementById('number_of_people').value;
-	pizzas = document.getElementById('number_of_pizza').value;
-	pizzaSize = document.getElementsByName('pizzasize').value;
+function calculate() {
+    var costPerPerson;
+    var totalCost;
+    var people;
+    var pizzas;
+    var slicesPerPizzas;
+    var slicesPerPerson;
+    var pizzaSize;
+    var pizzaSlices;
+    var slicesPerPerson;
 
 
-	costPerPerson = totalCost/people;
-	slicesPerPerson = (pizzas * slicesPerPizzas) / people;
-	
-	
-	document.getElementById('costPerPerson').innerHTML = costPerPerson;
-	
+    totalCost = document.getElementById('total_cost').value;
+    people = document.getElementById('number_of_people').value;
+    pizzas = document.getElementById('number_of_pizza').value;
+    pizzaSize = document.getElementsByName('pizzasize').value;
 
-	
-	
 
-	costPerPerson = "Cost Per Person: " + (totalCost/people);
+    costPerPerson = totalCost / people;
+    slicesPerPerson = (pizzas * slicesPerPizzas) / people;
 
-	
 
-	if (pizzaSize == small) {
-		pizzaSlices = 8;
+    document.getElementById('costPerPerson').innerHTML = costPerPerson;
 
-	} else if (pizzaSize == large) {
-		pizzaSlices = 16;
 
-	} else {
-		pizzaSlices = 12;
-	}
 
-	costPerPerson = (totalCost/people).toFixed(2);
 
-	slicesPerPerson = "Slices Per Person: " + ((pizzaSlices * pizzas) /people).toFixed(0);
-	
-	document.getElementById('costPerPerson').innerHTML = costPerPerson;
-	document.getElementById('slicePerPerson').innerHTML = slicesPerPerson;
+
+    costPerPerson = "Cost Per Person: " + (totalCost / people);
+
+
+
+    if (pizzaSize === small) {
+        pizzaSlices = 8;
+
+    } else if (pizzaSize === large) {
+        pizzaSlices = 16;
+
+    } else {
+        pizzaSlices = 12;
+    }
+
+    costPerPerson = (totalCost / people).toFixed(2);
+
+    slicesPerPerson = "Slices Per Person: " + ((pizzaSlices * pizzas) / people).toFixed(0);
+
+    document.getElementById('costPerPerson').innerHTML = costPerPerson;
+    document.getElementById('slicePerPerson').innerHTML = slicesPerPerson;
 }
