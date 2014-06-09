088faf35ed229e21a45d673c1602b34a4a70be54
Aaron Smith <aaronwsmith12@gmail.com>
2014-06-09 14:54:17 -0600

    javascript loops and bug fixes

--- pizza.js
+++ pizza.js
@@ -1,49 +1,158 @@
-function calculate() {
-    var costPerPerson;
-    var totalCost;
-    var people;
-    var pizzas;
-    var slicesPerPizzas;
-    var slicesPerPerson;
-    var pizzaSize;
-    var pizzaSlices;
-    var slicesPerPerson;
+< !DOCTYPE html > < html > < head > < title > pizza calculator < /title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
 
+  <link rel="stylesheet" type="text/css
+" href="
+pizza_calculator.css ">
+  <script type="
+text / javascript " src="
+pizza.js "></script>
+  <body>
+    <h1>Pizza Calculator</h1>
+    <section class="
+info ">
+      <ul>
+<<<<<<< HEAD
+        <li><label for="
+number_of_people ">How many people are getting pizza</label></br>
+      <select id="
+people_drop ">
+        <option>ONE</option>
+        <option>TWO</option>
+        <option>THREE</option>
+      </select>
+        <input type="
+text " name="
+number_of_people " id="
+number_of_people " required></br>
+        <li><label for="
+total_cost ">How much did the pizza cost</label></br></li>
+      <input type="
+text " name="
+total_cost " id="
+total_cost " required></br>
+      <li><label for="
+number_of_pizza ">How many pizza's did you buy</label></br>
+      <select id="
+pizza_drop ">
+        <option>ONE</option>
+        <option>TWO</option>
+        <option>THREE</option>
+      </select>
+      <input type="
+text " name="
+number_of_pizza " id="
+number_of_pizza " required></br>
+      <label for="
+pizzaSize ">What size do you want</label></br>
 
-    totalCost = document.getElementById('total_cost').value;
-    people = document.getElementById('number_of_people').value;
-    pizzas = document.getElementById('number_of_pizza').value;
-    pizzaSize = document.getElementsByName('pizzasize').value;
+      <input type="
+radio " name="
+pizzasize " id="
+small " value="
+small " class="
+pizza_size ">
+      <label for="
+small ">small</label>
 
+      <input type="
+radio " name="
+pizzasize " id="
+medium " value="
+medium " class="
+pizza_size ">
+      <label for="
+medium ">medium</label>
 
-    costPerPerson = totalCost / people;
-    slicesPerPerson = (pizzas * slicesPerPizzas) / people;
+      <input type="
+radio " name="
+pizzasize " id="
+large " value="
+large " class="
+pizza_size ">
+      <label for="
+large ">large</label></br>
+=======
+        <li><label for="
+number_of_people ">How many people are getting pizza</label></br>
+          <select>
+            <option>ONE</option>
+            <option>TWO</option>
+            <option>THREE</option>
+          </select>
+          <input type="
+text " name="
+number_of_people " id="
+number_of_people " required></br>
+          <li><label for="
+total_cost ">How much did the pizza cost</label></br></li>
+          <input type="
+text " name="
+total_cost " id="
+total_cost " required></br>
+          <li><label for="
+number_of_pizza ">How many pizza's did you buy</label></br>
+            <select>
+              <option>ONE</option>
+              <option>TWO</option>
+              <option>THREE</option>
+            </select>
+            <input type="
+text " name="
+number_of_pizza " id="
+number_of_pizza " required></br>
+            <label for="
+pizzaSize ">What size do you want</label></br>
 
+            <input type="
+radio " name="
+pizzasize " id="
+small " value="
+small ">
+            <label for="
+small ">small</label>
 
-    document.getElementById('costPerPerson').innerHTML = costPerPerson;
+            <input type="
+radio " name="
+pizzasize " id="
+medium " value="
+medium ">
+            <label for="
+medium ">medium</label>
 
+            <input type="
+radio " name="
+pizzasize " id="
+large " value="
+large ">
+            <label for="
+large ">large</label></br>
+>>>>>>> d61d1a3b1c31141362a769799f453684d74009a7
 
-
-
-
-    costPerPerson = "Cost Per Person: " + (totalCost / people);
-
-
-
-    if (pizzaSize === small) {
-        pizzaSlices = 8;
-
-    } else if (pizzaSize === large) {
-        pizzaSlices = 16;
-
-    } else {
-        pizzaSlices = 12;
-    }
-
-    costPerPerson = (totalCost / people).toFixed(2);
-
-    slicesPerPerson = "Slices Per Person: " + ((pizzaSlices * pizzas) / people).toFixed(0);
-
-    document.getElementById('costPerPerson').innerHTML = costPerPerson;
-    document.getElementById('slicePerPerson').innerHTML = slicesPerPerson;
-}
+            <!--<a href="
+pizzaimg.jpg " class="
+btn ">Calculate</a>-->
+    <div>
+            <input type="
+image " src="
+pizzaimg.jpg " width="
+100 " height="
+100 " name="
+saveForm " class="
+button " id="
+saveForm " onclick="
+calculate()
+" />
+    </div>
+            <!--<button type="
+button " onclick="
+calculate()
+"> calculate</button>-->
+            <div id="
+costPerPerson "></div>
+            <div id="
+slicePerPerson "></div>
+          </section>
+        </body>
+        </html>
